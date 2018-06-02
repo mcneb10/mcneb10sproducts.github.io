2203fcd1718e2d7b532984dd20038cc05e999cfb
mcneb10 <37050201+mcneb10@users.noreply.github.com>
2018-06-02 07:56:31 -0500

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+function multiply(x,y) {
+var result = x  * y;
+return result;
+}
+function divide(x,y) {
+var result1 = x / y;
+var result2 = x % y;
+return result1 + ' r:' + result2;
+}
+function add(x,y) {
+var result = x + y;
+return result;
+}
+function subtract(x,y) {
+var result = x - y;
+return result;
+}
